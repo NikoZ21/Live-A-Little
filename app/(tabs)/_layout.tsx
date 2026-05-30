d7e97d5779b6af5cfddbaae604b551d7164c0589
nambroladze <n.ambroladze@mziuri.ge>
2026-05-31 01:43:20 +0400

    basic tabs wired

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { Tabs } from "expo-router";
+import { Book, Compass, History, User } from "lucide-react-native";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#FF5A36" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Discover",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Compass color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="storybook"
+        options={{
+          title: "Storybook",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <Book color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <History color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => <User color={color} size={size} />,
+        }}
+      />
+    </Tabs>
+  );
+}
